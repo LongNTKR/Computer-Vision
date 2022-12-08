bcb35954fc2128e9fbb78a0d284a0e2eac924aa3
LongNTKR <longnt1306@gmail.com>
2022-12-08 20:05:56 +0700

    project

---
+++ Sign Digits Recognition/HandSignDigitsRecognition.py
@@ -0,0 +1,24 @@
+import HandTracking as ht
+import cv2
+
+def main():
+    detector = ht.HandDectector()
+    
+    cap = cv2.VideoCapture(0)
+    while True:
+        _, img = cap.read()
+        img = cv2.flip(img, 1)
+        
+        img = detector.find_hands_land_mark(img, draw=False)
+        
+        lmList = detector.find_position(img, draw=False)
+        if len(lmList) != 0:
+            img = detector.sign_digits_reconition(img)
+        
+        cv2.imshow('Image', img)
+        cv2.waitKey(1)
+
+
+if __name__ == '__main__':
+    main()
+
